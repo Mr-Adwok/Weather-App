56c796e690196717c5ad9b238ef771227355a799
Mr-Adwok <adwokbrave@gmail.com>
2026-05-24 08:55:58 +0200

    Add:Route to fetch city data

---
+++ app/routes/city_route.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter
+from settings import config
+import httpx
+
+
+
+weatherRouter = APIRouter()
+weather_api_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
+
+
+
+@weatherRouter.get('/weather/{city}')
+async def get_city_info(city:str):
+    url = f"{weather_api_url}/{city}?key={config.SECRET_KEY}"
+    async with httpx.AsyncClient() as conn:
+        response = await conn.get(url)
+        json_data = response.json()
+        print(response.status_code)
+
+
+    # print(type(get_city_info,"Hello tere "))
+    return {"response":json_data}
+
+
+
+#  so you
+
+
+
