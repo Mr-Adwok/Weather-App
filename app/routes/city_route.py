b9f3295584dd23f643901fe2a16b1349e2c8b05e
Mr-Adwok <adwokbrave@gmail.com>
2026-07-14 11:13:55 +0200

    Add: the function the measures execution time, caching logic with redis for perfomance

--- app/routes/city_route.py
+++ app/routes/city_route.py
@@ -1,6 +1,12 @@
 from fastapi import APIRouter
-from settings import config
+from ..settings import config
+from app.services.service  import measure_execution_time
+from app.redis import redis_client
+
+
 import httpx
+import json
+
 
 
 
@@ -10,8 +16,16 @@ weather_api_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/
 
 
 @weatherRouter.get('/weather/{city}')
+@measure_execution_time
 async def get_city_info(city:str):
     url = f"{weather_api_url}/{city}?key={config.SECRET_KEY}"
+    cache_key = f"weather:{city}"
+     # Check if the data exists in cache
+    cached_data = await redis_client.get(cache_key)
+    if cached_data:
+        print(cached_data)
+        return {"source": "cache", "data": cached_data}
+
     async with httpx.AsyncClient() as conn:
         response = await conn.get(url)
         json_data = response.json()
@@ -20,19 +34,21 @@ async def get_city_info(city:str):
         address = json_data.get('address')
         latitude = json_data.get('latitude');
         longitude = json_data.get('longitude')
+        # print(response.status_code)
+        result = {
+            "address":address,
+            "description":description,
+            "datetime":datetime,
+            "latitude":latitude,
+            "longitude":longitude
 
-        print(response.status_code)
+        }
+        dict_json = json.dumps(result)
+        await redis_client.setex(cache_key,600,dict_json)
 
 
     # print(type(get_city_info,"Hello tere "))
-    return {"response":{
-        'address':address,
-        'description':description,
-        'datetime':datetime,
-        'latitude':latitude,
-        'longitude':longitude
-
-    }}
+    return {"response":result}
 
 
 
