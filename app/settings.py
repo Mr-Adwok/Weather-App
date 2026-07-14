d22f207253a2c1b7a35350e64c02c022ccdc68b3
Mr-Adwok <adwokbrave@gmail.com>
2026-07-14 11:10:03 +0200

    Add: redis port and host from env to settings

--- app/settings.py
+++ app/settings.py
@@ -1,18 +1,18 @@
 from pydantic_settings import BaseSettings,SettingsConfigDict
 
-
-
-
 class Setting(BaseSettings):
 
     SECRET_KEY:str
+    REDIS_HOST:str = "localhost"
+    REDIS_PORT:int
 
     model_config = SettingsConfigDict(
-        env_file=".env"
+        env_file=".env",
+        extra = "ignore"
     )
 
 
 config = Setting()
 
 
-# print(config.SECRET_KEY)
+print(config.SECRET_KEY,"helloooo -------------------------------------------------------------------------------------------")
